b728e3383a62f1aca99b2fa500029ca504fd566e
echandiesteban <70446092+echandiesteban@users.noreply.github.com>
2022-09-18 12:56:03 -0400

    Add files via upload

--- NLPTest.py
+++ NLPTest.py
@@ -10,8 +10,14 @@ print(cwd)
 import pandas as pd
  
 # read by default 1st sheet of an excel file
-df = pd.read_excel('TestExcel.xlsx')
- 
+df = pd.read_excel('TestExcel5.xlsx')
+
+#from nltk.corpus import stopwords
+#stop = stopwords.words('english')
+#df["Text_wo"] = df['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+#df["Text_wo"]= df["Text_wo"].str.replace(r'\W'," ")
+
+
 print(df)
 
 print(df.label.unique())
@@ -24,9 +30,12 @@ print(df.isnull().sum())
 data = df.dropna()
 
 # Frecuencies
+counts = data['label2'].value_counts()
+counts.plot(kind='bar', legend=False, grid=True, figsize=(8, 5))
 
 counts = data['label'].value_counts()
 counts.plot(kind='bar', legend=False, grid=True, figsize=(8, 5))
+
 
 import numpy as np
 
@@ -37,14 +46,16 @@ lens.hist(bins = np.arange(0,200,5))
 
 import spacy_sentence_bert
 
-
-nlp = spacy_sentence_bert.load_model('en_stsb_distilbert_base')
+nlp = spacy_sentence_bert.load_model('en_stsb_bert_large')
+#nlp = spacy_sentence_bert.load_model('en_stsb_distilbert_base')
 
 df['vector'] = df['text'].apply(lambda x: nlp(x).vector)
 
-
 #split the data into training and testing sets
 
+
+
+#**************LABEL***************
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(df['vector'].tolist(), df['label'].tolist(), test_size=0.1, random_state=690)
@@ -52,29 +63,94 @@ X_train, X_test, y_train, y_test = train_test_split(df['vector'].tolist(), df['l
 # train your choice of machine learning classifier
 
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 
+from sklearn.metrics import accuracy_score
 clf = SVC(gamma='auto')
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 print(accuracy_score(y_test, y_pred))
 
 from sklearn.ensemble import RandomForestClassifier
-
 clf = RandomForestClassifier(max_depth=9, random_state=0)
 clf.fit(X_train, y_train)
-
 y_pred = clf.predict(X_test)
 print(accuracy_score(y_test, y_pred))
 
+from sklearn.metrics import classification_report
+print(classification_report(y_test,y_pred))
+
+
+
+#**************LABEL2***************
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(df['vector'].tolist(), df['label2'].tolist(), test_size=0.1, random_state=690)
+
+# train your choice of machine learning classifier
+from sklearn.svm import SVC
+
+from sklearn.metrics import accuracy_score
+clf = SVC(gamma='auto')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(max_depth=9, random_state=0)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test,y_pred))
+
+
+
+
+
+
+
+
 #Test Algorithm
 
-Examples = ["thrust levers","release","the pilot","pf","PF","if the crosswind is at or below 20 knots and there is no tailwind :","in case of tailwind or if crosswind is greater than 20 knots:"]
+Examples = ['takeoff', 'if the crosswind' ,'Pilot']
 
-label = ["Action (Where)", "Action (How)","Actor","Actor","Actor","Trigger (What)","Decide (What)"]
+label = ['Action (What)', 'Decide (What)' ,'Actor']
 
 for Examples, label in zip(Examples, label):
   print(Examples,)
   print(f"True Label: {label}, Predicted Label: {clf.predict(nlp(Examples).vector.reshape(1, -1))[0]} \n")
+
+# Visualizaion
+
+import numpy as np
+
+from sklearn.manifold import TSNE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
